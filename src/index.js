68ed91dc4bccd39837540fcc950fbb6e7e1d9918
Diego Oberto <diegooberto9@gmail.com>
2025-04-08 10:15:07 -0400

    testing logic

--- src/index.js
+++ src/index.js
@@ -1,28 +1,68 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
 import odinImage from "./assets/odin.jpg";
-console.log(greeting);
+import { getDate } from "date-fns";
+///console.log(greeting);
 const user = { name: 'Alice', age: 25 };
 
-class post {
-    constructor(post,tasks,is_done,date_made){
+class Post {
+    constructor(post,description,is_done,date_made,priority){
         this.post = post;
-        this.tasks = tasks;
+        this.description = description;
         this.is_done = is_done;
         this.date_made = date_made;
-    }
-    save(){
-        const json_string
+        this.priority = priority
+    };
+    getstringnojson(){
+        const normal_string = { "post" : this.post,
+                              "description" : this.description,
+                              "is_done" : this.is_done,
+                              "date_made" : this.date_made,
+                              "priority" : this.priority
+        }
+        return normal_string;
     }
 }
+class project {
+   constructor(project){
+    this.project = project;
+
+   };
+   setproject(){
+    const normal_string = {"project" : this.project,
+                            "posts" : []
+    };
+    localStorage.setItem(`${this.project}`,JSON.stringify(normal_string));
+   };
+    makeapostonproject(post,description,is_done,date_made,priority){
+    let newpost = new Post (post,description,is_done,date_made,priority);
+    let dedo = JSON.parse(localStorage.getItem(`${this.project}`));
+   dedo.posts.push(newpost.getstringnojson());
+   localStorage.setItem(`${this.project}`, JSON.stringify(dedo));
+    
+
+}
+}
+    
+   
+
+const prueba1 = new project("cero");
+prueba1.setproject();
+
+prueba1.makeapostonproject("loco","unmocnddkjfkdkd",true,"azul",3);
+prueba1.makeapostonproject("tres","asdadsa",true,"verde",4);
 
 
-// Retrieve the object
-const storedUser = JSON.parse(localStorage.getItem('user'));
-console.log(storedUser.age); // Output: { name: 'Alice', age: 25 }
-console.log(JSON.parse(localStorage.getItem(0)));
-console.log(localStorage.length)
-//const image = document.createElement("img");
-//image.src = odinImage;
+console.log(JSON.parse(localStorage.getItem("cero")))
+function RetrieveList(){
+    let list =[];
+    for (let i=0; i < localStorage.length;i++){
+        const key = localStorage.key(i);
+        const item = JSON.parse(localStorage.getItem(key));
+        list.push({item});
+    }
+    return list;
+}
+
    
 //document.body.appendChild(image);
